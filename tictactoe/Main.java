34f69028768972f11dfe587afe2e2f893518d365
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-01-25 11:33:52 -0600

    :stage 1/5: print mock grid

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,5 +1,8 @@
 package tictactoe;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         var grid = new Grid();
@@ -9,23 +12,35 @@ public class Main {
 }
 
 class Grid {
-    int m;
-    int n;
+    int[][] grid;
 
     Grid(){
         this(3,3);
     } 
     
     Grid(int rows, int columns) {
-        m = rows;
-        n = columns;
+        if (rows <= 0 && columns <= 0) throw new IllegalArgumentException();
+        grid = new int[rows][columns];
     }
 
     void autoFill() {
-        System.out.println("Autofilling grid");
+        var random = new Random();
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = random.nextBoolean() ? 1 : 0;
+            }
+        }
     }
 
     void print() {
-        System.out.println("Printing grid");
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) { 
+               String character =grid[i][j] == 0 ? "O" :
+                                    grid[i][j] == 1 ? "X" :
+                                        "-"; 
+               System.out.print(character + " ");
+           }
+           System.out.println();
+        } 
     }
 }
